9458c29d98e6f0314980293650afd3cdfd6cc9f7
Nael Alolwani <naelalolwani@gmail.com>
2021-06-05 17:37:07 +0800

    Scope class and Authorize method

--- lib/Net/Async/Spotify.pm
+++ lib/Net/Async/Spotify.pm
@@ -7,4 +7,108 @@ use warnings;
 
 # ABSTRACT: Interaction with the spotify.com API 
 
+use parent qw(IO::Async::Notifier);
+
+use Future::AsyncAwait;
+use Syntax::Keyword::Try;
+use Log::Any qw($log);
+use Net::Async::HTTP;
+use URI;
+use URI::QueryParam;
+use Math::Random::Secure qw(irand);
+
+use Net::Async::Spotify::Scope qw(:all);
+
+sub configure_unknown {
+    my ($self, %args) = @_;
+    for my $k (grep exists $args{$_}, qw(client_id client_secret redirect_uri auth_code access_token refresh_token base_uri)) {
+        $self->{$k} = $args{$k};
+    }
+}
+
+sub _add_to_loop {
+    my $self = shift;
+    $self->add_child($self->{http} = Net::Async::HTTP->new(
+            fail_on_error            => 1,
+            close_after_request      => 0,
+            max_connections_per_host => 2,
+            pipeline                 => 1,
+            max_in_flight            => 4,
+            decode_content           => 1,
+            stall_timeout            => 15,
+            user_agent               => 'Mozilla/4.0 (perl; Net::Async::Spotify; VNEALV@cpan.org)',
+        ));
+}
+
+sub base_uri { shift->{base_uri} //= URI->new('https://accounts.spotify.com/') }
+sub http { shift->{http} }
+
+sub client_id     { shift->{client_id} }
+sub client_secret { shift->{client_secret} }
+sub redirect_uri  { shift->{redirect_uri} //= URI->new('http://localhost/callback') }
+sub auth_code     { shift->{auth_code} }
+
+sub access_token  { shift->{access_token} }
+sub refresh_token { shift->{refresh_token} }
+
+=head1 METHODS
+
+=head2 authorize
+
+Returns URI object with it being the needed Spotify Authorization request along with the needed parameters set.
+also return random hexadecimal number as the state attached to this request.
+https://developer.spotify.com/documentation/general/guides/authorization-guide/ Step 1 in Authorization Code Flow.
+Accepts limited named parameters
+
+=over 4
+
+=item client_id
+Spotify ClientID, set to class client_id if not passed.
+
+=item response_type
+set as `code` for default. Since Authorization Code Flow is used.
+
+=item redirect_uri
+URI string that will be used as Authorization callback URL. Set to main app redirect_url if not peresnt.
+
+=item state
+Used as linking mechanism between this generated Authorize Request, and the incoming callback code response.
+will be set to a random hexadecimal number.
+
+=item scope
+Sets permissions to be requested. Accepts array of scopes or scopes categories.
+e.g. scope => [app_remote_control', 'user-follow-read', 'spotify_connect']
+
+=item show_dialog
+optional param can be passed set to either true|false
+
+=back
+
+=cut
+
+sub authorize {
+    my ($self, %args) = @_;
+    my $allowed_params = join '|', qw(client_id response_type redirect_uri state scope show_dialog);
+
+    $args{client_id} //= $self->client_id;
+    $args{response_type} //= 'code';
+    $args{redirect_uri} //= $self->redirect_uri->as_string;
+    $args{state} //= sprintf '%08x', irand(2**32);
+    $args{scope} = join ' ', map {  Net::Async::Spotify::Scope->$_ } $args{scope}->@* if exists $args{scope};
+
+    # Don't leak any extra params
+    delete $args{$_} for grep { !/$allowed_params/ } keys %args;
+
+    my $auth_uri = URI->new($self->base_uri . 'authorize');
+    $auth_uri->query_param($_, $args{$_}) for keys %args;
+    return (uri => $auth_uri, state => $args{state});
+    # request has to be viewed on browser and accepted.
+    #
+    # my $r = await $self->http->do_request(uri => $auth_uri, method => 'GET');
+    # $log->warnf('rr %s', $r->content);
+    # Once accepted redirect_url will be called with authorization code passed.
+}
+
+
+
 1;
